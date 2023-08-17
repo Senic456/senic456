627a850cdf9331e28581339c0a28d9f3cc42b811
ericburnett456 <ebgm_e@outlook.com>
2023-08-17 13:51:39 -0400

    reset

---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+
+import { FaSignOutAlt } from 'react-icons/fa';
+import { FaGoogle } from 'react-icons/fa';
+function Logout ({logout}){
+    return(
+        <nav className="navbar">
+    <div className="navbar-left">
+    <span className="message-space"><h4>ChatSpace:P</h4></span>
+    </div>
+    <div className="navbar-right">
+    <button className="logout-button" onClick={logout}>
+        Logout <FaSignOutAlt />
+    </button>
+    </div>
+</nav>
+        
+    )
+}
+ export default Logout
+
